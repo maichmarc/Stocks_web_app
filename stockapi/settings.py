d35a21d56506a1814b5791f7a28c75959842d29c
maichmarc <maichmarc@gmail.com>
2025-11-13 12:28:46 +0300

    Settings

--- stockapi/settings.py
+++ stockapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^4up(1)ljfa3@e2733%&p^7z#+zby+&c%^3&1cm=u8qc78ftp6"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
